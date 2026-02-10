2c8c56b8e40e40c748aebd7fdc8a346af271b014
Garychamp <garyhoare18@gmail.com>
2026-02-10 19:32:37 +0000

    Updates made to html, css and javascript files

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const container = document.getElementById('container');
 
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  container.appendChild(square);
+}
